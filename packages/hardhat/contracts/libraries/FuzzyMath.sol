65250c0220d73c0a7e8378a790331216ca8059f3
JBLee <brianlee2795@gmail.com>
2022-05-11 01:11:43 -0700

    Gas cost improvements for FuzzyMath

--- packages/hardhat/contracts/libraries/FuzzyMath.sol
+++ packages/hardhat/contracts/libraries/FuzzyMath.sol
@@ -11,34 +11,37 @@ library FuzzyMath {
 
     /**
      * @dev estimates the equation f(x) = x^(a/b)^
-     * NOTE: bounding of the z estimate could be improved and would both reduce
-     *  the computation cost and allow for larger inputs. Overall, bounds and
-     *  initial guesses need some work, considering 1e9 ** 9 is an overflow..
+     * does so by calculating x^a^ and iterating to the b-root of that subtotal
+     * using a generalized form of the Babylonian Method
+     * citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.569.8009&rep=rep1&type=pdf
+
+     * NOTE: greater inputs could be considered if this function were used to
+     * estimate exponential mappings with a limited number of sig-figs
      */ 
     function fraxExp(uint x, uint8 a, uint8 b) external pure returns (uint est)  {
         // constraints
         require(b < 10, "need exponent denominator b < 10");
         require(a < 10, "need exponent numerator a < 10");
-        require(x < 1000000000, "maximum input is 1e9");
+        require(x < 500000000, "maximum input is 5e9");
 
-        if (a % b == 0) return x ** (a / b); // shortcut this nonsense
-        if (b % a == 0) (a, b) = (1, b / a); // simplify
-        x = (a > 1) ? x**a : x;              // calculate subtotal with exponent numerator        
+        if (x <= 1) return x;                   // shortcut this nonsense
+        if (a % b == 0) return x ** (a / b);    // and this nonsense
+        if (b % a == 0) (a, b) = (1, b / a);    // simplify
+        uint subTotal = (a > 1) ? x**a : x;     // calculate subtotal with exponent numerator        
 
-        // calculate b-root of subtotal with generalized Babylonian Method
-        // citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.569.8009&rep=rep1&type=pdf
-        uint z = (x + 1) / 2; // intial guess, could be improved
-        if (b == 2) {         // simplified square root case
-            est = x;
+        // iteratively estimate b-root of subtotal
+        uint z;
+        est = subTotal;
+        if (b == 2) {               // simplified square root case
+            z = (subTotal + 1) / 2; // intial guess, could be improved
             while (z < est) {
-                (est, z) = (z, (x / z + z) / 2);
+                (est, z) = (z, (subTotal / z + z) / 2);
             }
         } else {
-            uint b2 = b - 1;
-            z = (z > 1000000000) ? z : 1000000000; // potentially tighter guess, answer bounded by 1e9
-            est = x;
+            uint8 b2 = b - 1;
+            z = x**(a/b + 1) - 1;   // z < est, always
             while (z < est) {
-                (est, z) = (z, (x / z**b2  + b2 * z) / b);
+                (est, z) = (z, (subTotal / z**b2 + b2 * z) / b);
             }
         }
     }
