c2773653ed38b052d7a396468dfb6e604c6b36b7
JBLee <brianlee2795@gmail.com>
2022-05-21 23:14:12 -0700

    Cleanup Tests for Fuzzy
    Also fixes est mistake on lower numbers. Can revisit optimizations later

--- packages/hardhat/contracts/libraries/FuzzyMath.sol
+++ packages/hardhat/contracts/libraries/FuzzyMath.sol
@@ -19,7 +19,7 @@ library FuzzyMath {
      * estimate exponential mappings with a limited number of sig-figs
      */ 
     function fraxExp(uint x, uint8 a, uint8 b) external view returns (uint ans)  {
-        // constraints
+        // constraints (too loose rn.. 1e9^9^ is overflow)
         require(b < 10, "need exponent denominator b < 10");
         require(a < 10, "need exponent numerator a < 10");
         require(x < 1000000000, "maximum input is 1e9");
@@ -28,35 +28,33 @@ library FuzzyMath {
         if (a % b == 0) return x ** (a / b);    // and this nonsense
         if (b % a == 0) (a, b) = (1, b / a);    // simplify
 
-        uint subTotal = (a > 1) ? x**a : x;     // calculate subtotal with exponent numerator, prior to rooting
+        uint subTotal = (a > 1) ? x**a : x;     // calc subtotal w exp numerator, prior to rooting
         if (b == 2) return sqrt(subTotal);      // shortcut square root case
 
-        // figure out tight bound to answer 
-        // TODO: make it tighter
-        uint temp = subTotal;
-        uint8 subTotalMSB = 0;
-        while(temp > 0) {
-            subTotalMSB++;
-            temp = temp>>1;
-        }
-        uint guessBoundExp = subTotalMSB / b;
-        guessBoundExp += (subTotalMSB % b != 0) ? 1 : 0; // round exp up if not perfect power of 2
-        uint z = 2**guessBoundExp;
-
-        // console.log("z:", z, "subTotal:", subTotal);
+        // calculate tight upper bound on answer
+        // bound:= 2^c^ ; where c = ⌈⌈lg2(subTotal)⌉ / b⌉
+        // TODO: tighten bound; optimize MSB check w binary search (max 256 loops currently lol)
+        uint8 subTotalMSB;
+        while(subTotal>>subTotalMSB > 0) subTotalMSB++;
+        uint expBoundGuess = subTotalMSB / b;
+        expBoundGuess += (subTotalMSB % b != 0) ? 1 : 0;
+        uint z = (1<<expBoundGuess) - 1;
 
         uint count = 0;
         uint8 b2 = b - 1;
-        ans = subTotal;                         // initial guess for answer
+        ans = subTotal;
+        console.log("z:", z);
         while (z < ans) {
             count++;
             (ans, z) = (z, (subTotal / z**b2 + b2 * z) / b);
         }
-        console.log("num loops:",count);
+        console.log("ans:", ans);
+        console.log("num loops:", count);
     }
 
     /**
-     * @dev estimates the square root of an input
+     * @dev estimates the square root of an input using Babylonian Method
+     * https://www.cs.utep.edu/vladik/2009/olg09-05a.pdf
      */
     function sqrt(uint x) public pure returns (uint ans) {
         ans = x;
--- packages/hardhat/test/FuzzyMathTest.js
+++ packages/hardhat/test/FuzzyMathTest.js
@@ -8,15 +8,12 @@ use(solidity);
 function fibonacciArray(n) {
   let list = new Array(n);
   list[0] = 1;
+  if (n > 1) list[1] = 1;
+  for(i = 2; i < n; i++) list[i] = list[i-1] + list[i-2];
 
-  if (n > 1) {
-    list[1] = 1;
-    for(i = 2; i < n; i++) list[i] = list[i-1] + list[i-2];
-  }
   return list;
 }
-
-let fibs = fibonacciArray(27);
+let fibs = fibonacciArray(100);
 
 describe("Testing FuzzyMath", function () {
   let fuzzyMath;
@@ -29,7 +26,6 @@ describe("Testing FuzzyMath", function () {
 
   it("Perfect Squares", async function () {
     let n, square;
-    
     for (let i = 1; fibs[i] < 3 * 10 ** 4; i++) {
       n = fibs[i];
       square = n ** 2;
@@ -38,44 +34,47 @@ describe("Testing FuzzyMath", function () {
     }
   });
 
-  it("Perfect Cubic Roots", async function () {
+  it("Perfect Cube Roots", async function () {
     let n, cube;
-    
     for (let i = 1; fibs[i] < 10 ** 3; i++) {
       n = fibs[i];
       cube = n ** 3;
-      console.log("\tinput: ", n)
+      // console.log("\tinput: ", n)
       expect(await fuzzyMath.fraxExp(cube, 1, 3)).to.equal(n);
     }
   });
 
+  it("Imperfect Square Roots", async function () {
+    let guess, index = 1, input = fibs[1];
+    while (input < 10 ** 9) {
+      guess = Math.floor(Math.sqrt(input));
+      // console.log("input:", input, "\tguess:", guess);
+      expect(await fuzzyMath.fraxExp(input, 1, 2)).to.equal(guess);      
+      input = fibs[++index];
+    }
+  });
+
+  it("Imperfect Cube Roots", async function () {
+    let guess, index = 1, input = fibs[1];
+    while (input < 10 ** 9) {
+      guess = Math.floor(Math.cbrt(input));
+      // console.log("input:", input, "\tguess:", guess);
+      expect(await fuzzyMath.fraxExp(input, 1, 3)).to.equal(guess);      
+      input = fibs[++index];
+    }
+  });
+
   it("Perfect Roots w Arbitrary Fractional Exponents", async function () {
-    // test all valid single-digit pairs of a and b on i^b for i [2,9]
     let input;
     for (let b = 2; b < 10; b++) {
       // console.log(`testing perfect ${b}-roots for all values a..`);
       for (let a = 1; a < 10; a++) {
         for (let i = 1; i < 10; i++) {
           input = i ** b;
-          console.log(`\tb: ${b}, a: ${a}, input: ${input}`);
+          // console.log(`\tb: ${b}, a: ${a}, input: ${input}`);
           expect(await fuzzyMath.fraxExp(input, a, b)).to.equal(i ** a);
         }
       }
     }
   });
-
-  // it("single test", async function () {
-  //   expect(await fuzzyMath.fraxExp(4782969, 8, 7)).to.equal(8);
-  // });
-  it("Square Roots w Numbers (imperfect)", async function () {
-    for (let n = 1; n < 10 ** 9; n += n) {
-      console.log("n:", n);
-      let guess = Math.floor(Math.sqrt(n));
-      // console.log("guess:", guess);
-      expect(await fuzzyMath.fraxExp(n, 1, 2)).to.equal(guess);
-    }
-  });
-
-
-
 });
