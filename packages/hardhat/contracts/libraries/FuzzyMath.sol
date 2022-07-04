bc291e405658748e7eff36f608997253c50e6675
JBLee <brianlee2795@gmail.com>
2022-07-03 21:37:03 -0700

    Fuzzy Math Optimizations
    Cutting gas down with some unchecked math and assignment separation

--- packages/hardhat/contracts/libraries/FuzzyMath.sol
+++ packages/hardhat/contracts/libraries/FuzzyMath.sol
@@ -19,14 +19,21 @@ library FuzzyMath {
      * estimate exponential mappings with a limited number of sig-figs
      */ 
     function fraxExp(uint x, uint8 a, uint8 b) external view returns (uint ans)  {
-        // constraints (too loose rn.. 1e9^9^ is overflow)
+        // constraints (too loose rn.. 1e81 is overflow)
         require(b < 10, "need exponent denominator b < 10");
         require(a < 10, "need exponent numerator a < 10");
         require(x < 1000000000, "maximum input is 1e9");
 
-        if (x <= 1) return x;                   // shortcut this nonsense
-        if (a % b == 0) return x ** (a / b);    // and this nonsense
-        if (b % a == 0) (a, b) = (1, b / a);    // simplify
+        // shortcuts n simpifications (224 gas) -- do we need these?
+        unchecked {
+            if (x <= 1) return x;
+            if (a % b == 0) return x ** (a / b);
+            if (b % a == 0) {   // (122 gas) for this alone
+                a = 1;
+                b = b / a;
+                // (a, b) = (1, b / a);  // not sure why but inline assignment costs 10 extra gas
+            }
+        }
 
         uint subTotal = (a > 1) ? x**a : x;     // calc subtotal w exp numerator, prior to rooting
         if (b == 2) return sqrt(subTotal);      // shortcut square root case
@@ -34,22 +41,21 @@ library FuzzyMath {
         // calculate tight upper bound on answer
         // bound:= 2^c^ ; where c = ⌈⌈lg2(subTotal)⌉ / b⌉
         // TODO: tighten bound; optimize MSB check w binary search (max 256 loops currently lol)
-        uint8 subTotalMSB;
+        uint8 subTotalMSB = 0;
         while(subTotal>>subTotalMSB > 0) subTotalMSB++;
         uint expBoundGuess = subTotalMSB / b;
         expBoundGuess += (subTotalMSB % b != 0) ? 1 : 0;
         uint z = (1<<expBoundGuess) - 1;
 
-        uint count = 0;
         uint8 b2 = b - 1;
         ans = subTotal;
-        console.log("z:", z);
         while (z < ans) {
-            count++;
-            (ans, z) = (z, (subTotal / z**b2 + b2 * z) / b);
+            unchecked {
+                ans = z;
+                z = (subTotal / z**b2 + b2 * z) / b;
+                // (ans, z) = (z, (subTotal / z**b2 + b2 * z) / b);
+            }
         }
-        console.log("ans:", ans);
-        console.log("num loops:", count);
     }
 
     /**
